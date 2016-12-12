863886760cdd8927c7440277e0b2869cc84a0dd2
Christian Simon <simon@swine.de>
2016-12-12 20:29:24 +0000

    Test

--- main.go
+++ main.go
@@ -18,25 +18,76 @@ type GPSElement struct {
 	Distance float64
 }
 
-func main() {
+type World struct {
+	socket *websocket.Conn
+}
+
+type Command struct {
+	Contents string `json:"contents"`
+	Tag      string `json:"tag"`
+}
+
+func (w *World) CommandTag(tag, content string) error {
+	c := &Command{
+		Tag:      tag,
+		Contents: content,
+	}
+
+	bytes, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	bytes = append(bytes, byte('\n'))
+
+	_, err = w.socket.Write(bytes)
+	return err
+}
+
+func NewWorld(url string) (*World, error) {
 	origin := "http://localhost/"
-	url := "ws://game.clearercode.com"
 	ws, err := websocket.Dial(url, "", origin)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	//if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
-	//	log.Fatal(err)
-	//}
-	var msg = make([]byte, 1024)
-	var n int
-	if n, err = ws.Read(msg); err != nil {
-		log.Fatal(err)
-	}
-	var game Game
-	err = json.Unmarshal(msg[:n], &game)
+
+	w := &World{socket: ws}
+
+	return w, nil
+}
+
+func main() {
+	w, err := NewWorld("ws://game.clearercode.com")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Infof("%+v.\n", game)
+
+	err = w.CommandTag("SetName", "team-golang")
+	if err != nil {
+		log.Fatal("error setting name: ", err)
+	}
+
+	err = w.CommandTag("SetColor", "ffff00")
+	if err != nil {
+		log.Fatal("error setting color: ", err)
+	}
+
+	var game Game
+	buffer := make([]byte, 65535)
+	for {
+		n, err := w.socket.Read(buffer)
+		if err != nil {
+			log.Warn("End of input: ", err)
+			break
+		}
+
+		line := buffer[:n]
+		log.Infof("line: %+v", string(line))
+		err = json.Unmarshal(line, &game)
+		if err != nil {
+			log.Warn("error parsing message: ", err)
+			continue
+		}
+		log.Infof("message: %+v", game)
+	}
 }
