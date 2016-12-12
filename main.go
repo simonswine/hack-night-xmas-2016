ee9be4af4cd85c30bfe83391685f0f1db69904ea
Christian Simon <simon@swine.de>
2016-12-12 20:55:05 +0000

    Changes

--- main.go
+++ main.go
@@ -4,22 +4,35 @@ import (
 	"encoding/json"
 	log "github.com/Sirupsen/logrus"
 	"golang.org/x/net/websocket"
+	"sync"
 )
 
 type Game struct {
-	GPSS []GPSElement
+	GPSS    []GPSElement
+	Players []Player
+}
+
+type Player struct {
+	Position Position
+	Score    int
+	Color    string
+	Name     string
+}
+
+type Position struct {
+	X float64
+	Y float64
 }
 
 type GPSElement struct {
-	Position struct {
-		X int
-		Y int
-	}
+	Position Position
 	Distance float64
 }
 
 type World struct {
-	socket *websocket.Conn
+	socket   *websocket.Conn
+	encoder  *json.Encoder
+	position Position
 }
 
 type Command struct {
@@ -27,21 +40,56 @@ type Command struct {
 	Tag      string `json:"tag"`
 }
 
+func (w *World) Encode(v interface{}) error {
+	return w.encoder.Encode(v)
+}
+
 func (w *World) CommandTag(tag, content string) error {
-	c := &Command{
+	log.WithField("tag", tag).WithField("content", content)
+	c := Command{
 		Tag:      tag,
 		Contents: content,
 	}
+	return w.Encode(&c)
+}
 
-	bytes, err := json.Marshal(c)
+func (w *World) Run() {
+	w.encoder = json.NewEncoder(w.socket)
+	err := w.CommandTag("SetName", "team-golang")
 	if err != nil {
-		return err
+		log.Fatal("error setting name: ", err)
 	}
 
-	bytes = append(bytes, byte('\n'))
+	err = w.CommandTag("SetColor", "#00ffff")
+	if err != nil {
+		log.Fatal("error setting color: ", err)
+	}
 
-	_, err = w.socket.Write(bytes)
-	return err
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var game Game
+		decoder := json.NewDecoder(w.socket)
+		for {
+			err := decoder.Decode(&game)
+			if err != nil {
+				log.Warn("error parsing message: ", err)
+				continue
+			}
+			log.Debugf("game: %+v", game)
+			for _, player := range game.Players {
+				if player.Name == "team-golang" {
+					w.position = player.Position
+					break
+				}
+			}
+			log.Infof("my position: %+v", w.position)
+		}
+	}()
+
+	wg.Wait()
 }
 
 func NewWorld(url string) (*World, error) {
@@ -61,33 +109,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	err = w.CommandTag("SetName", "team-golang")
-	if err != nil {
-		log.Fatal("error setting name: ", err)
-	}
-
-	err = w.CommandTag("SetColor", "ffff00")
-	if err != nil {
-		log.Fatal("error setting color: ", err)
-	}
-
-	var game Game
-	buffer := make([]byte, 65535)
-	for {
-		n, err := w.socket.Read(buffer)
-		if err != nil {
-			log.Warn("End of input: ", err)
-			break
-		}
-
-		line := buffer[:n]
-		log.Infof("line: %+v", string(line))
-		err = json.Unmarshal(line, &game)
-		if err != nil {
-			log.Warn("error parsing message: ", err)
-			continue
-		}
-		log.Infof("message: %+v", game)
-	}
+	w.Run()
 }
